aa9983232624778446578848253385a9ea7c610e
Faustin Date <22149223+faustind@users.noreply.github.com>
2024-04-30 22:00:29 +0900

    Add hello and goodbye routes

--- config/routes.rb
+++ config/routes.rb
@@ -2,5 +2,7 @@ Rails.application.routes.draw do
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
 
   # Defines the root path route ("/")
-  root 'application#goodbye'
+  root 'application#hello'
+  get 'hello', to: 'application#hello'
+  get 'goodbye', to: 'application#goodbye'
 end
